d881f1b5dade59d8ed517a1b2e62b5205e08db3f
zhouqiang-cl <zhouqiang.cl@gmail.com>
2020-04-19 23:34:38 +0800

    add benchmark

--- README.md
+++
@@ -1,2 +0,0 @@
-# stability
-stability tests
---
+++ multi_connect.sh
@@ -0,0 +1,9 @@
+for i in `seq 1 1000`;
+do
+    for j in `seq 1 1000`;
+    do
+        tablename=`date +%s`
+        mysql -h 127.0.0.1 -P 4000 -u root -e "create database if not exists multi; use multi;CREATE TABLE t$tablename(c0 INT);"
+    done
+    sleep 100
+done
